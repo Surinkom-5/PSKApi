59794d0ddaef49a38969e17400ea8b5bc8f39a51
volungeviciute <laima.volungeviciute@gmail.com>
2021-04-22 11:30:52 +0300

    created order repository

---
+++ src/FlowerShop.Infrastructure/Repositories/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using FlowerShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Infrastructure.Repositories
+{
+    interface IOrderRepository
+    {
+        public Task<Order> GetOrderByIdAsync(int orderId);
+        public Task<List<Order>> GetOrdersByUserIdAsync(int userId);
+    }
+}

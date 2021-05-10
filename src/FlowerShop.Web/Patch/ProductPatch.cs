d567a1b0b3f8bf321638b7c94f319c92de8c74d8
volungeviciute <laima.volungeviciute@gmail.com>
2021-05-10 20:59:42 +0300

    Added Product Patch class

---
+++ src/FlowerShop.Web/Patch/ProductPatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Web.Patch
+{
+    public class ProductPatch
+    {
+        public int ProductId { get; set; }
+        public int? Quantity { get; set; }
+        public String Description { get; set; }
+        public String Name { get; set; }
+        public decimal? Price { get; set; }
+    }
+}

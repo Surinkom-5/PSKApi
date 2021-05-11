8fe5f4ae73345e3804e69237ea09b3c7aa16c090
Augustas Masolas <augissmaso@gmail.com>
2021-05-12 00:00:14 +0300

    logger

--- src/FlowerShop.Web/Program.cs
+++ src/FlowerShop.Web/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using System;
 
 namespace FlowerShop.Web
@@ -13,27 +14,41 @@ namespace FlowerShop.Web
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
             {
-                var services = scope.ServiceProvider;
+                logger.Debug("init main function");
+                var host = CreateHostBuilder(args).Build();
 
-                try
+                using (var scope = host.Services.CreateScope())
                 {
-                    var context = services.GetRequiredService<AppDbContext>();
-                    context.Database.Migrate();
-                    context.Database.EnsureCreated();
-                    SeedData.Initialize(services);
+                    var services = scope.ServiceProvider;
+
+                    try
+                    {
+                        var context = services.GetRequiredService<AppDbContext>();
+                        context.Database.Migrate();
+                        context.Database.EnsureCreated();
+                        SeedData.Initialize(services);
+                    }
+                    catch (Exception ex)
+                    {
+                        var loggerService = services.GetRequiredService<ILogger<Program>>();
+                        loggerService.LogError(ex, "An error occurred seeding the DB.");
+                    }
                 }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
+
+                host.Run();
             }
-
-            host.Run();
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error in init");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -45,9 +60,10 @@ namespace FlowerShop.Web
                     .ConfigureLogging(logging =>
                 {
                     logging.ClearProviders();
-                    logging.AddConsole();
+                    logging.SetMinimumLevel(LogLevel.Information); // Overriden by appsettings
                     // logging.AddAzureWebAppDiagnostics(); add this if deploying to Azure
-                });
+                })
+                    .UseNLog();
             });
 
     }

be3f18a0f7763c778378054ae75f0978edbaf7d5
Tautvydas Jackevičius <tautvydas.jackevicius@gmail.com>
2021-05-14 21:12:52 +0300

    Modified ProductViewModel

--- src/FlowerShop.Web/Models/ProductViewModel.cs
+++ src/FlowerShop.Web/Models/ProductViewModel.cs
@@ -6,21 +6,25 @@ namespace FlowerShop.Web.Models
 {
     public class ProductViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
         public string ProductType { get; set; }
-        public int AvailabilityCount { get; set; }
+        public int Quantity { get; set; }
+        public string Image { get; set; }
 
         public static ProductViewModel ToModel(Product product)
         {
             return new ProductViewModel
             {
+                Id = product.ProductId,
                 Name = product.Name,
                 Price = product.Price,
                 Description = product.Description,
                 ProductType = product.ProductType.ToString(),
-                AvailabilityCount = product.AvailabilityCount
+                Quantity = product.AvailabilityCount,
+                Image = product.ImageUrl
             };
         }
 
